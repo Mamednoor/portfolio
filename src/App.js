1a9fb07dfcc4327c4cae27519b1da089f7f056be
Mamednoor <mamed.gohabur@gmail.com>
2020-04-13 03:05:50 +0200

    in progress stop for today

--- src/App.js
+++ src/App.js
@@ -10,29 +10,29 @@ class App extends Component {
   render () {
     return (
       <div className="demo-big-content">
-    <Layout>
-        <Header title="Menu" fixed>
-            <Navigation>
-                <Link to="/"><a href="/">Home</a></Link>
-                <Link to="/about"><a href="/about">About</a></Link>
-                <Link to="/dashboard"><a href="/dashboard">Porte Folio</a></Link>
-                <Link to="/contact"><a href="/contact">Contact</a></Link>
-            </Navigation>
-        </Header>
-        <Drawer title="Menu">
-            <Navigation>
-              <Link to="/"><a href="/">Home</a></Link>
-              <Link to="/about"><a href="/about">About</a></Link>
-              <Link to="/dashboard"><a href="/dashboard">Porte Folio</a></Link>
-              <Link to="/contact"><a href="/contact">Contact</a></Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main />
-        </Content>
-    </Layout>
-</div>
+        <Layout>
+            <Header title="Menu" fixed>
+                <Navigation>
+                    <Link to="/"><a className="link" href="/">Home</a></Link>
+                    <Link to="/about"><a className="link" href="/about">About</a></Link>
+                    <Link to="/dashboard"><a className="link" href="/dashboard">Porte Folio</a></Link>
+                    <Link to="/contact"><a className="link" href="/contact">Contact</a></Link>
+                </Navigation>
+            </Header>
+            <Drawer title="Menu">
+                <Navigation>
+                  <Link to="/"><a className="linkaside" href="/">Home</a></Link>
+                  <Link to="/about"><a className="linkaside" href="/about">About</a></Link>
+                  <Link to="/dashboard"><a className="linkaside" href="/dashboard">Porte Folio</a></Link>
+                  <Link to="/contact"><a className="linkaside" href="/contact">Contact</a></Link>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <Main />
+            </Content>
+        </Layout>
+      </div>
     )
 }
 }
