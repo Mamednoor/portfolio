a92b8af8cf3de8d4200d51ec5f89f1b29eb6f418
Mamednoor <mamed.gohabur@gmail.com>
2020-04-13 11:30:35 +0200

    i did it without framework, just for button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import NavLink from './components/pages/NavLink'
 import Main from './components/pages/Main'
-import {Layout, Header, Navigation, Drawer, Content} from "react-mdl"
-import {Link} from 'react-router-dom'
+
 
 import './App.css';
 
@@ -9,32 +9,34 @@ import './App.css';
 class App extends Component {
   render () {
     return (
-      <div className="demo-big-content">
-        <Layout>
-            <Header title="Menu" fixed>
-                <Navigation>
-                    <Link to="/"><a className="link" href="/">Home</a></Link>
-                    <Link to="/about"><a className="link" href="/about">About</a></Link>
-                    <Link to="/dashboard"><a className="link" href="/dashboard">Porte Folio</a></Link>
-                    <Link to="/contact"><a className="link" href="/contact">Contact</a></Link>
-                </Navigation>
-            </Header>
-            <Drawer title="Menu">
-                <Navigation>
-                  <Link to="/"><a className="linkaside" href="/">Home</a></Link>
-                  <Link to="/about"><a className="linkaside" href="/about">About</a></Link>
-                  <Link to="/dashboard"><a className="linkaside" href="/dashboard">Porte Folio</a></Link>
-                  <Link to="/contact"><a className="linkaside" href="/contact">Contact</a></Link>
-                </Navigation>
-            </Drawer>
-            <Content>
-                <div className="page-content" />
-                <Main />
-            </Content>
-        </Layout>
+      <div >
+        <NavLink />
+        <Main/>
       </div>
     )
 }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/components/pages/NavLink.js
+++ src/components/pages/NavLink.js
@@ -1,27 +1,32 @@
-const li = [
-    {
-        link: "/",
-        text: "Home"
-    },
-    {
-        link: "/about/",
-        text: "Présentation"
-    },
-    {
-        link: "/dashboard/",
-        text: "Porte Folio"
-    },
-    {
-        link: "/contact/",
-        text: "Contact"
-    }
-];
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { FaAlignRight } from 'react-icons/fa'
+import '../stylesheet/NavLink.css'
 
-const NavLink = () => (
-
-    <div>
-        {li.map(objLink => (<App link={objLink.link} text={objLink.text} />))}
-    </div>
-)
+class NavLink extends Component {
+state = {
+    toggle:false
+}
+Toggle = () => {
+    this.setState({toggle:!this.state.toggle})
+}
+render() {
+    return (
+    <header>
+        <div className="navBar">
+            <button onClick={this.Toggle}>
+                <FaAlignRight />
+            </button>
+            <ul className={this.state.toggle ? "links show-nav" : "links"}>
+                <Link to="/"><li className="link" href="/">Home</li></Link>
+                <Link to="/about"><li className="link" href="/about">Présentation</li></Link>
+                <Link to="/dashboard"><li className="link" href="/dashboard">Porte Folio</li></Link>
+                <Link to="/contact"><li className="link" href="/contact">Contact</li></Link>
+            </ul>
+        </div>
+    </header>
+    );
+}
+}
 
 export default NavLink;
