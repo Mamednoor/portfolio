14ea4523d05184283ebfbff5945ca577461923ab
Mamednoor <mamed.gohabur@gmail.com>
2020-06-01 12:15:32 +0200

    suppression image de fond

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavLink from './components/shared/NavLink'
 import Main from './components/shared/Main';
-import BackImg from '../assets/background.png'
 import './App.css';
 
 
-class App extends Component {
-  render () {
+function App() {
     return (
       <div>
         <NavLink />
         <Main />
-        <img className='FixBackGround' src={BackImg} alt='logo'/>
       </div>
     )
 }
-}
+
 
 export default App;
 
