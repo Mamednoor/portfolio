4f1f293e996990d221993eaa1ca5528141e5fc53
Mamednoor <mamed.gohabur@gmail.com>
2020-06-08 02:57:11 +0200

    correction index.js et ajout package.json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import { BrowserRouter } from "react-router-dom";
-
-import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
