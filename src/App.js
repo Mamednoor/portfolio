b25d8fb747f72db292064d6b101b72883e3020e8
Mamednoor <mamed.gohabur@gmail.com>
2020-06-10 09:47:31 +0200

    ajout de quelque changement et une page supplémentaire

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Home from "./components/pages/Home";
 import Info from "./components/pages/Info";
@@ -6,20 +6,20 @@ import Experience from "./components/pages/Experience";
 import PortFolio from "./components/pages/PortFolio";
 import Follow from "./components/pages/Follow";
 import NavBar from "./components/shared/NavBar";
+import Stack from "./components/pages/Stack";
 
-class App extends Component {
-  render() {
+function App() {
     return (
       <div className="App">
         <NavBar />
         <Home id="Home" />
         <Info id="Info" />
+        <Stack />
         <PortFolio id="PorteFolio" />
         <Experience id="Experience" />
         <Follow id="Contact" />
       </div>
     );
   }
-}
 
 export default App;
---
+++ src/components/pages/Stack.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import logoReact from "../assets/img/react.svg";
+import logoNode from "../assets/img/nodejs.png";
+import logoJS from "../assets/img/Js.png";
+import logoMail from "../assets/img/";
+import logoHtml from "../assets/img/Html.png";
+import logoCss from "../assets/img/CSS.png";
+import logoGit from "../assets/img/Git.png";
+import logoGithub from "../assets/img/Github.png";
+import logoLinkedin from "../assets/img/Linkedin.png";
+import logoUbuntu from "../assets/img/Ubuntu.png";
+import logoMySQL from "../assets/img/Mysql.png";
+import logoCV from '../assets/img/CV.png'
+
+{/* <img src={logoReact} alt="React">React</img>
+<img className="img-card" alt="ScreenProject" src={logoJS} /> */}
+
+function Stack() {
+  return (
+    <div>
+      <ul>
+                <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
+                <li><a href="https://twitter.com/ddbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
+                <li><a href="https://www.instagram.com/ddbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+                <li><a href="https://www.linkedin.com/in/dbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
+              </ul>
+    </div>
+  );
+}
+
+export default Stack;
