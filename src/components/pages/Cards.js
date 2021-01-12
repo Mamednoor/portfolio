6b4b8f3342d35521ab0ac64324b3afe8f2f71728
Mamednoor <mamed.gohabur@gmail.com>
2021-01-13 00:35:32 +0100

    save progress

--- src/components/pages/Cards.js
+++ src/components/pages/Cards.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-import cookcake from "../assets/img/CookAndCake.png";
-import goatvschicken from "../assets/img/GoatvsChicken.png";
-import doctalk from "../assets/img/doctalk.jpg";
-import pixieWilder from "../assets/img/PixieWilders.png";
-import reduxtrain from "../assets/img/redux-train.png";
-import papiercrayon from "../assets/img/papiercrayon.png";
+// import cookcake from "../assets/img/CookAndCake.png";
+// import goatvschicken from "../assets/img/GoatvsChicken.png";
+// import doctalk from "../assets/img/doctalk.jpg";
+// import pixieWilder from "../assets/img/PixieWilders.png";
+// import reduxtrain from "../assets/img/redux-train.png";
+// import papiercrayon from "../assets/img/papiercrayon.png";
 
 import project from "../../utils/project";
 
