c3bd4f027a450e2056217a351b00a200c6499771
Mamednoor <mamed.gohabur@gmail.com>
2021-06-26 20:29:43 +0200

    SP, change img link in project.js and delete line preview

--- src/utils/project.js
+++ src/utils/project.js
@@ -2,8 +2,7 @@ const project = [
   {
     "name": "Pixie Wilders",
     "description": " - Création d’une marketplace de photographie",
-    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/components/assets/img/PixieWilders.png?raw=true",
-    "preview": "preview",
+    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/PixieWilders.png",
     "stack": " - Stack: HTML, CSS, Git et Github",
     "link": "https://mamednoor.github.io/project1PixieWilder/",
     "repo": "https://github.com/Mamednoor/project1PixieWilder"
@@ -11,8 +10,7 @@ const project = [
   {
     "name": "Cook&Cake",
     "description": " - Création d’une Application web de cuisine avec consommation d'API",
-    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/components/assets/img/CookAndCake.png?raw=true",
-    "preview": "preview",
+    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/CookAndCake.png",
     "stack": " - Stack: React, JavaScript, HTML, CSS, Git et Github, SCRUM",
     "link": "https://cookandcake.netlify.app/",
     "repo": "https://github.com/Mamednoor/paris-0320-project2-Cook-Cake"
@@ -20,8 +18,7 @@ const project = [
   {
     "name": "Goat Vs Chicken",
     "description": " - Création d'un jeux vidéo lors d'un Hackathon de 24H à la WildCodeSchool Paris",
-    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/components/assets/img/GoatvsChicken.png?raw=true",
-    "preview": "preview",
+    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/GoatvsChicken.png",
     "stack": " - Stack: JavaScript, Phaser3, Git et Github",
     "link": "https://romantic-swirles-0e45a7.netlify.app/",
     "repo": "https://github.com/Mamednoor/HackathonProject"
@@ -29,8 +26,7 @@ const project = [
   {
     "name": "Doc'Talk",
     "description":" - Création d'une application mobile mettant en relation médecins et patients. Hackathon Doctolib 2020",
-    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/components/assets/img/doctalk.png?raw=true",
-    "preview": "preview",
+    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/doctalk.png",
     "stack": " - Stack: MySQL, React, Node.js, Socket.io, Git & GitHub, SCRUM",
     "link": "https://doctalk.netlify.app/",
     "repo": "https://github.com/Mamednoor/doctalkfusion"
@@ -38,8 +34,7 @@ const project = [
   {
     "name": "Papier Crayon",
     "description": " - Création d’une marketplace de design Papier crayon",
-    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/components/assets/img/papiercrayon.png?raw=true",
-    "preview": "preview",
+    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/papiercrayon.png",
     "stack": " - Stack: MySQL, React, Node.js, Git & GitHub, SCRUM",
     "link": "https://papier-crayon.netlify.app/",
     "repo": "https://github.com/Mamednoor/papiercrayon"
@@ -47,8 +42,7 @@ const project = [
   {
     "name": "Todo List",
     "description": " - Création d’une Todo List avec Redux",
-    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/components/assets/img/redux-train.png?raw=true",
-    "preview": "preview",
+    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/redux-train.png",
     "stack": " - Stack: React + Redux, Git & Github",
     "link": "https://redux-train-todoapp.netlify.app/",
     "repo": "https://github.com/Mamednoor/redux-train"
@@ -56,8 +50,7 @@ const project = [
   {
     "name": "Pokedex with Angular",
     "description": " - Création d'un mini pokedex",
-    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/components/assets/img/pokedex.png?raw=true",
-    "preview": "preview",
+    "image": "https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/pokedex.png",
     "stack": " - Stack: HTML, CSS, JavaScript, Angular",
     "link": "https://github.com/Mamednoor/pokedexAngular",
     "repo": "https://github.com/Mamednoor/pokedexAngular"
