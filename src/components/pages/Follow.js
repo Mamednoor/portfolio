2ba7c86f24782e22c188d29babe48371cb97c2da
Mamednoor <mamed.gohabur@gmail.com>
2021-11-01 13:50:55 +0100

    useless CHange

--- src/components/pages/Follow.js
+++ src/components/pages/Follow.js
@@ -1,71 +1,63 @@
-import React from "react";
+import React from 'react'
 
 import {
 	MailOutlined,
 	LinkedinOutlined,
 	GithubOutlined,
-} from "@ant-design/icons";
+} from '@ant-design/icons'
 
-import Link from "../common/Link";
-import Bottom from "../common/Bottom";
-import Title from "../common/Title";
+import Link from '../common/Link'
+import Bottom from '../common/Bottom'
+import Title from '../common/Title'
 
-import "../stylesheet/Follow.css";
+import '../stylesheet/Follow.css'
 
 function Follow() {
 	return (
 		<>
-			<Title id='Follow'>
-				Suivez-moi
-			</Title>
+			<Title id="Follow">Suivez-moi</Title>
 			<Bottom>
 				<Link
-					href='https://fr.linkedin.com/in/mamednoorgohabur'
-					target='_blank'
-					rel='noopener noreferrer'
+					href="https://fr.linkedin.com/in/mamednoorgohabur"
+					target="_blank"
+					rel="noopener noreferrer"
 				>
 					<LinkedinOutlined
 						style={{
-							fontSize:
-								"70px",
-							color:
-								"white",
+							fontSize: '70px',
+							color: 'white',
 						}}
-						className='icons'
+						className="icons"
 					/>
 				</Link>
 				<Link
-					href='https://github.com/Mamednoor'
-					target='_blank'
-					rel='noopener noreferrer'
+					href="https://github.com/Mamednoor"
+					target="_blank"
+					rel="noopener noreferrer"
 				>
 					<GithubOutlined
 						style={{
-							fontSize:
-								"70px",
-							color:
-								"white",
+							fontSize: '70px',
+							color: 'white',
 						}}
-						className='icons'
+						className="icons"
 					/>
 				</Link>
 				<Link
-					href='mailto:mamed.gohabur@gmail.com'
-					target='_blank'
-					rel='noopener noreferrer'
+					href="mailto:mamed.gohabur@gmail.com"
+					target="_blank"
+					rel="noopener noreferrer"
 				>
 					<MailOutlined
 						style={{
-							fontSize:
-								"70px",
-							color:
-								"white",
+							fontSize: '70px',
+							color: 'white',
 						}}
-						className='icons'
+						className="icons"
 					/>
 				</Link>
 			</Bottom>
 		</>
-	);
+	)
 }
-export default Follow;
+export default Follow
