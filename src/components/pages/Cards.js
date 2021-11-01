aec628b614e3ada5b950a44846d3b956c0d7cdb3
Mamednoor <mamed.gohabur@gmail.com>
2021-11-01 11:50:45 +0100

    [FIX], change image propertie on project.js

--- src/components/pages/Cards.js
+++ src/components/pages/Cards.js
@@ -1,50 +1,37 @@
-import React from "react";
+import React from 'react'
 
-// import cookcake from "../../assets/img/CookAndCake.png";
-// import goatvschicken from "../../assets/img/GoatvsChicken.png";
-// import doctalk from "../../assets/img/doctalk.jpg";
-// import pixieWilder from "../../assets/img/PixieWilders.png";
-// import reduxtrain from "../../assets/img/redux-train.png";
-// import papiercrayon from "../../assets/img/papiercrayon.png";
-// import pokedex from "../../assets/img/pokedex.png";
-
-
-import project from "../../utils/project"
+import project from '../../utils/project'
 import Link from '../common/Link'
-import { CardBoxed, CardContainer, CustomImage, CustomCard, Img, P } from '../common/Card'
-
+import {
+	CardBoxed,
+	CardContainer,
+	CustomImage,
+	CustomCard,
+	Img,
+	P,
+} from '../common/Card'
 
 function Cards() {
-  return (
-    <>
-      {project.map((item, i) => (
-        <CardContainer 
-          key={i}
-          className="card-container"
-        >
-          <CardBoxed>
-            <CustomImage>
-              <Img 
-                alt="ScreenProject"
-                src={item.image}
-              />
-            </CustomImage>
-            <CustomCard>
-              <Link
-                href={item.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h3>{item.name}</h3>
-              </Link>
-                <P>{item.description}</P>
-                <P>{item.stack}</P>
-            </CustomCard>
-          </CardBoxed>
-        </CardContainer>
-      ))}
-    </>
-  );
+	return (
+		<>
+			{project.map((item, i) => (
+				<CardContainer key={i} className="card-container">
+					<CardBoxed>
+						<CustomImage>
+							<Img alt="ScreenProject" src={item.image} />
+						</CustomImage>
+						<CustomCard>
+							<Link href={item.link} target="_blank" rel="noopener noreferrer">
+								<h3>{item.name}</h3>
+							</Link>
+							<P>{item.description}</P>
+							<P>{item.stack}</P>
+						</CustomCard>
+					</CardBoxed>
+				</CardContainer>
+			))}
+		</>
+	)
 }
 
-export default Cards;
+export default Cards
--- src/utils/project.js
+++ src/utils/project.js
@@ -1,94 +1,98 @@
-const project =
-	[
-		{
-			name: "Pixie Wilders",
-			description:
-				" - Création d’une marketplace de photographie",
-			image:
-				"https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/PixieWilders.png?raw=true",
-			stack:
-				" - Stack: HTML, CSS, Git et Github",
-			link: "https://mamednoor.github.io/project1PixieWilder/",
-			repo: "https://github.com/Mamednoor/project1PixieWilder",
-		},
-		{
-			name: "Cook&Cake",
-			description:
-				" - Création d’une Application web de cuisine avec consommation d'API",
-			image:
-				"https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/CookAndCake.png?raw=true",
-			stack:
-				" - Stack: React, JavaScript, HTML, CSS, Git et Github, SCRUM",
-			link: "https://cookandcake.netlify.app/",
-			repo: "https://github.com/Mamednoor/paris-0320-project2-Cook-Cake",
-		},
-		{
-			name: "Goat Vs Chicken",
-			description:
-				" - Création d'un jeux vidéo lors d'un Hackathon de 24H à la WildCodeSchool Paris",
-			image:
-				"https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/GoatvsChicken.png?raw=true",
-			stack:
-				" - Stack: JavaScript, Phaser3, Git et Github",
-			link: "https://romantic-swirles-0e45a7.netlify.app/",
-			repo: "https://github.com/Mamednoor/HackathonProject",
-		},
-		{
-			name: "Doc'Talk",
-			description:
-				" - Création d'une application mobile mettant en relation médecins et patients. Hackathon Doctolib 2020",
-			image:
-				"https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/doctalk.png?raw=true",
-			stack:
-				" - Stack: MySQL, React, Node.js, Socket.io, Git & GitHub, SCRUM",
-			link: "https://doctalk.netlify.app/",
-			repo: "https://github.com/Mamednoor/doctalkfusion",
-		},
-		{
-			name: "Papier Crayon",
-			description:
-				" - Création d’une marketplace de design Papier crayon",
-			image:
-				"https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/papiercrayon.png?raw=true",
-			stack:
-				" - Stack: MySQL, React, Node.js, Git & GitHub, SCRUM",
-			link: "https://papier-crayon.netlify.app/",
-			repo: "https://github.com/Mamednoor/papiercrayon",
-		},
-		{
-			name: "Todo List",
-			description:
-				" - Création d’une Todo List avec Redux",
-			image:
-				"https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/redux-train.png?raw=true",
-			stack:
-				" - Stack: React + Redux, Git & Github",
-			link: "https://redux-train-todoapp.netlify.app/",
-			repo: "https://github.com/Mamednoor/redux-train",
-		},
-		{
-			name: "Worker & Co",
-			description:
-				" - Création d’un site vitrine pour une agence de co-working",
-			image:
-				"https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/Worker&Co.png?raw=true",
-			stack:
-				" - Stack: HTML, CSS, JavaScript, Bootstrap, Git & Github",
-			link: "https://workerandco.netlify.app",
-			repo: "https://github.com/Mamednoor/project-wbf3",
-		},
+import cookcake from '../assets/img/CookAndCake.png'
+import goatvschicken from '../assets/img/GoatvsChicken.png'
+import doctalk from '../assets/img/doctalk.jpg'
+import pixieWilder from '../assets/img/PixieWilders.png'
+import reduxtrain from '../assets/img/redux-train.png'
+import papiercrayon from '../assets/img/papiercrayon.png'
+// import pokedex from '../assets/img/pokedex.png'
+import workerCo from '../assets/img/Worker&Co.png'
+import portfoliov2 from '../assets/img/portefolio-php.png'
 
-		{
-			name: "Portefolio v2",
-			description:
-				"Portefolio",
-			image:
-				"https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/portefolio-php.png?raw=true",
-			stack:
-				" - Stack: HTML, CSS, JavaScript, PHP",
-			link: "https://mamedgohabur.42web.io/",
-			repo: "https://github.com/Mamednoor/Portfolio_PHP",
-		},
-	];
+const project = [
+	{
+		name: 'Pixie Wilders',
+		description: ' - Création d’une marketplace de photographie',
+		// image:
+		// 	'https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/PixieWilders.png?raw=true',
+		image: `${pixieWilder}`,
+		stack: ' - Stack: HTML, CSS, Git et Github',
+		link: 'https://mamednoor.github.io/project1PixieWilder/',
+		repo: 'https://github.com/Mamednoor/project1PixieWilder',
+	},
+	{
+		name: 'Cook&Cake',
+		description:
+			" - Création d’une Application web de cuisine avec consommation d'API",
+		// image:
+		// 	'https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/CookAndCake.png?raw=true',
+		image: `${cookcake}`,
+		stack: ' - Stack: React, JavaScript, HTML, CSS, Git et Github, SCRUM',
+		link: 'https://cookandcake.netlify.app/',
+		repo: 'https://github.com/Mamednoor/paris-0320-project2-Cook-Cake',
+	},
+	{
+		name: 'Goat Vs Chicken',
+		description:
+			" - Création d'un jeux vidéo lors d'un Hackathon de 24H à la WildCodeSchool Paris",
+		// image:
+		// 	'https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/GoatvsChicken.png?raw=true',
+		image: `${goatvschicken}`,
+		stack: ' - Stack: JavaScript, Phaser3, Git et Github',
+		link: 'https://romantic-swirles-0e45a7.netlify.app/',
+		repo: 'https://github.com/Mamednoor/HackathonProject',
+	},
+	{
+		name: "Doc'Talk",
+		description:
+			" - Création d'une application mobile mettant en relation médecins et patients. Hackathon Doctolib 2020",
+		// image:
+		// 	'https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/doctalk.png?raw=true',
+		image: `${doctalk}`,
+		stack: ' - Stack: MySQL, React, Node.js, Socket.io, Git & GitHub, SCRUM',
+		link: 'https://doctalk.netlify.app/',
+		repo: 'https://github.com/Mamednoor/doctalkfusion',
+	},
+	{
+		name: 'Papier Crayon',
+		description: ' - Création d’une marketplace de design Papier crayon',
+		// image:
+		// 	'https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/papiercrayon.png?raw=true',
+		image: `${papiercrayon}`,
+		stack: ' - Stack: MySQL, React, Node.js, Git & GitHub, SCRUM',
+		link: 'https://papier-crayon.netlify.app/',
+		repo: 'https://github.com/Mamednoor/papiercrayon',
+	},
+	{
+		name: 'Todo List',
+		description: ' - Création d’une Todo List avec Redux',
+		// image:
+		// 	'https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/redux-train.png?raw=true',
+		image: `${reduxtrain}`,
+		stack: ' - Stack: React + Redux, Git & Github',
+		link: 'https://redux-train-todoapp.netlify.app/',
+		repo: 'https://github.com/Mamednoor/redux-train',
+	},
+	{
+		name: 'Worker & Co',
+		description: ' - Création d’un site vitrine pour une agence de co-working',
+		// image:
+		// 	'https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/Worker&Co.png?raw=true',
+		image: `${workerCo}`,
+		stack: ' - Stack: HTML, CSS, JavaScript, Bootstrap, Git & Github',
+		link: 'https://workerandco.netlify.app',
+		repo: 'https://github.com/Mamednoor/project-wbf3',
+	},
 
-export default project;
+	{
+		name: 'Portefolio v2',
+		description: 'Portefolio',
+		// image:
+		// 	'https://github.com/Mamednoor/Mamednoor/blob/master/src/assets/img/portefolio-php.png?raw=true',
+		image: `${portfoliov2}`,
+		stack: ' - Stack: HTML, CSS, JavaScript, PHP',
+		link: 'https://mamedgohabur.42web.io/',
+		repo: 'https://github.com/Mamednoor/Portfolio_PHP',
+	},
+]
+
+export default project
