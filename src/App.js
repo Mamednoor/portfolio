47be627a98e3afda86f5368bcdd79804a6623365
Mamednoor <mamed.gohabur@gmail.com>
2022-01-08 14:45:48 +0100

    adding particles on background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
-import "./App.css";
-import Info from "./components/pages/Info";
-import Experience from "./components/pages/Experience";
-import Portfolio from "./components/pages/Portfolio";
-import Follow from "./components/pages/Follow";
-import NavBar from "./components/shared/NavBar";
-import Home from "./components/pages/Home";
+import React from 'react'
+import './App.css'
+import Info from './components/pages/Info'
+import Experience from './components/pages/Experience'
+import Portfolio from './components/pages/Portfolio'
+import Follow from './components/pages/Follow'
+import NavBar from './components/shared/NavBar'
+import Home from './components/pages/Home'
+import Particle from './components/common/Particles'
 
 function App() {
-    return (
-      <div className="App">
-        <NavBar />
-        <Home id="Home" />
-        <Info id="Info" />
-        <Portfolio id="portefolio" />
-        <Experience id="Experience" />
-        <Follow id="Contact" />
-      </div>
-    );
-  }
+	return (
+		<div className="App">
+			<NavBar />
+			<Particle />
+			<Home id="Home" />
+			<Info id="Info" />
+			<Portfolio id="portefolio" />
+			<Experience id="Experience" />
+			<Follow id="Contact" />
+		</div>
+	)
+}
 
-export default App;
+export default App
