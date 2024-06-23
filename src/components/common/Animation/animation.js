e7e204ccbca09cb30fd69c4a982123f68f4e00a8
Mamednoor <mamed.gohabur@gmail.com>
2024-06-23 18:38:07 +0200

    correction

--- src/components/common/Animation/animation.js
+++ src/components/common/Animation/animation.js
@@ -4,28 +4,13 @@ import { init } from 'ityped'
 import H2 from '../H2'
 
 const Animation = () => {
-<<<<<<< HEAD
-  
-	/*
-  useEffect(() => {
-     const myElement = document.querySelector('#myElement')
-    init(myElement, { showCursor: false, strings: ['Développeur Fullstack', 'React, Node.js'] })
-  }, []) */
-	
-=======
->>>>>>> origin/master
 	useEffect(() => {
 		const myElement = document.querySelector('#myElement')
 		init(myElement, {
 			showCursor: false,
 			strings: [
-<<<<<<< HEAD
-				'Développeur Fullstack React, Node.js',
-				'Développeur Power Platform',
-=======
 				'Développeur Power Platform',
 				'Développeur Fullstack React, Node.js',
->>>>>>> origin/master
 			],
 		})
 	}, [])
